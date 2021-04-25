42fba36b042144db01f0d91e0290da2faab1fb1d
lunaticholic <lunaticholic@gmail.com>
2021-04-25 21:42:52 +0400

    1.8 React Hook Form is Awesome - 진짜 개쩌는 React Hook Form으로 만나는 로그인화면의 신박함

--- src/components/auth/Button.js
+++ src/components/auth/Button.js
@@ -9,6 +9,7 @@ text-align: center;
 padding: 8px 0px;
 font-weight: 600;
 width: 100%;
+opacity: ${(props) => props.disabled ? "0.2" : "1"}
 `;
 
 export default Button;
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -12,6 +12,7 @@ import { ImageBox } from "../components/auth/ImageBox";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
 import { useForm } from "react-hook-form";
+import FormError from "../components/auth/FormError";
 
 const FacebookLogin = styled.div`
     color: #385285;
@@ -25,11 +26,18 @@ function Login() {
     // input을 위해 state를 만들어줘야 하고, onChange도 만들고, value도 설정해야 하는데
     // register를 사용하면 모든 게 해결됨
     // watch를 사용하게 되면 현재 form에 기록되는 모든 글자를 바라볼 수 있음
-    const { register, handleSubmit } = useForm();
+    // handleSubmit은 폼에서 어떠한 양식이 작성되고 전달되면 그 전달되는 역할을 수행해준다.
+    // errors는 폼 안에서 일어나는 에러를 감지하고 메세지가 있다면 출력해준다.
+    // formState는 폼에서 일어날 수 있는 것들에 대해 반응한다
+    const { register, handleSubmit, errors, formState } = useForm({
+        mode: "onChange"
+    })
+    // onBlur는 어떠한 항목을 벗어났을때만 입력된 값이 유효한지 확인한다는 의미
+    // onChange는 어떠한 항목이 변경될 때마다 입력된 값이 유효한지 확인한다는 의미
 
     // onSubmitValid는 form에 있는 input을 검사해줌
     const onSubmitValid = (data) => { console.log(data) }
-    const onSubmitInValid = (data) => { console.log(data, "invalid") }
+    
     return (
         <AuthLayout>
             {/* 화면 상단 탭에 보이는 디자인 */}
@@ -41,12 +49,17 @@ function Login() {
                     </div>
                 </ImageBox>
                 {/* handleSumbit이 form을 검증할거임, 유효하면 onSubmitValid, 유효하지 않으면 onSubmitInValid */}
-                <form onSubmit={handleSubmit(onSubmitValid, onSubmitInValid)}>
+                <form onSubmit={handleSubmit( onSubmitValid )}>
                     {/* register안에 required는 필수항목, minLength는 최소로 입력되어야 할 문자의 수 */}
                     {/* ref안에는 validate를 사용하여 필수로 추가되어야 하는 것도 검사 가능하고, pattern을 통해 입력 형식을 검사할 수 있다. 개쩌네 */}
-                    <Input ref={register({ required: "username은 필수항목입니다.", minLength: 5 })} name="username" type="text"placeholder="USERNAME" />
-                    <Input ref={register({ required: "password는 필수항목입니다.", minLength: 8 })} name="password" type="password" placeholder="PASSWORD" />
-                    <Button type="submit" value="LOG IN" />
+                    <Input ref={register({ required: "username은 필수항목입니다.", minLength: { value: 5, message: "username은 최소 5글자 이상입니다."} })} name="username" type="text"placeholder="USERNAME" hasError={Boolean(errors?.username?.message)} />
+                    {/* username 채워져 있지 않은 경우 아래 항목을 통해 username required가 메세지로 보일 것이다. */}
+                    <FormError message={errors?.username?.message} />
+                    <Input ref={register({ required: "password는 필수항목입니다.", minLength: 8 })} name="password" type="password" placeholder="PASSWORD" hasError={Boolean(errors?.password?.message)} />
+                    {/* password항목이 채워져 있지 않은 경우 아래 항목을 통해 password required가 메세지로 보일 것이다. */}
+                    <FormError message={errors?.password?.message} />
+                    {/* disabled를 통해 이 form이 다 채워지기 전에는 활성화가 안되도록 설정한다. */}
+                    <Button type="submit" value="LOG IN" disabled={!formState.isValid} />
                 </form>
             <Separator />
             <FacebookLogin>
